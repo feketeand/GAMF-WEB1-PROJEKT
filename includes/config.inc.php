fd66cdc098a76159baba46862f7b1e45c62a7702
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-04-13 20:59:58 +0200

    Add config.inc.php

---
+++ includes/config.inc.php
@@ -0,0 +1,27 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Panel oldal',
+);
+
+$fejlec = array(
+    'kepforras' => 'kep1.jpg',
+	'cim' => 'Panelos oldal',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'oldal' => 'Panelos oldal'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Kezdőlap'),
+	'panellakasokrol' => array('fajl' => 'panellakasokrol', 'szoveg' => 'Panellakásokról általában'),
+	'lakotelepek' => array('fajl' => 'lakotelepek', 'szoveg' => 'Lakótelepek Budapesten'),
+	'lakaskereso' => array('fajl' => 'lakaskereso', 'szoveg' => 'Lakáskereső'),
+	'otletfeltolto' => array('fajl' => 'otletfelto', 'szoveg' => 'Ötlet feltöltés'),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat'),
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
