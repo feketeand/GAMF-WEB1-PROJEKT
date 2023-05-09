38ca76b32bf9d491eb6e5b57410dc28f912f969e
feketeand <130245144+feketeand@users.noreply.github.com>
2023-05-10 01:17:44 +0200

    Add files via upload

---
+++ templates/pages/belepes.tpl.php
@@ -0,0 +1,23 @@
+    <form action = "?oldal=belep2" method = "post">
+      <fieldset>
+        <legend>Bejelentkezés</legend>
+        <br>
+        <input type="text" name="felhasznaloNev" placeholder="felhasználó" required><br><br>
+        <input type="password" name="jelszo" placeholder="jelszó" required><br><br>
+        <input type="submit" name="belepes" value="Belépés">
+        <br>&nbsp;
+      </fieldset>
+    </form>
+    <h3>Regisztrálja magát, ha még nem felhasználó!</h2>
+    <form action = "?oldal=regisztral2" method = "post">
+    <fieldset>
+        <legend>Regisztráció</legend>
+        <br>
+        <input type="text" name="vezeteknev" placeholder="vezetéknév" required><br>
+        <input type="text" name="keresztnev" placeholder="keresztnév" required><br>
+        <input type="text" name="felhasznaloNev" placeholder="felhasználó név" required><br>
+        <input type="password" name="jelszo" placeholder="jelszó" required><br>
+        <input type="submit" name="regisztracio" value="Regisztráció">
+        <br>&nbsp;
+      </fieldset>
+    </form>
