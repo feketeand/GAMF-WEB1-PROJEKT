a56c031a8c75bdc0a5ab4b369704667f93913c9b
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-09 08:28:39 +0200

    Update cimlap.tpl.php

---
+++ templates/pages/cimlap.tpl.php
@@ -0,0 +1,203 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Panelprojekt</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</head>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<ul class="actions2">																		
+						<li><a href="belepes.html" jet.Depots class="button">Belépés/Regisztráció</a></li>
+					</ul>
+					
+					<nav>				
+						
+						<ul>
+							<li><a href="#intro">Kezdőoldal</a></li>
+							<li><a href="#one">Panellakásokról általában</a></li>
+							<li><a href="#two">Lakótelepek Budapesten</a></li>
+							<li><a href="#three">Lakáskereső</a></li>
+                            <li><a href="#four">Ötlet feltöltés</a></li>
+                            <li><a href="#five">Kapcsolat</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Panelprojekt</h1>
+							<p>Üdvözöljük</p>							
+						</div>
+					</section>
+
+				<!-- Panellakásokról -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="images/kep1.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2> leírás</h2>
+									<p>Szöveg</p>									
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/kép2.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>További cikkek a témában</h2>
+									<p>Link lista</p>									
+								</div>
+							</div>
+						</section>						
+					</section>
+
+				<!-- Lakótelep kereső -->
+					<section id="two" class="wrapper style2 spotlights">
+                        <section>
+							<a href="#" class="image"><img src="images/kep3.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Lakótelep kereső</h2>
+									<p>Szűrőfelület: kerület/lakótelep</p>
+									<ul class="actions">
+										<li><a href="lakotelep.html" class="button">Szűrés</a></li>
+									</ul>									
+								</div>
+							</div>
+						</section>						
+						
+						
+                    </section>
+
+
+                <!-- Lakáskereső -->
+					
+					<section id="three" class="wrapper style2 spotlights">
+                        <section>
+							<a href="#" class="image"><img src="images/kep3.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Lakástípus kereső</h2>
+									<p>Szűrőfelület: kerület/lakótelep/méret/szobaszám/külön wc/ablakos konyha</p>
+									<ul class="actions">
+										<li><a href="lakotelep.html" class="button">Szűrés</a></li>
+									</ul>									
+								</div>
+							</div>
+						</section>						
+						
+						
+                    </section>
+					
+				<!-- Ötlet feltöltése -->	
+					<section id="four" class="wrapper style2 spotlights">
+                        <section>
+							<a href="#" class="image"><img src="images/kep3.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Megvalósított ötlet feltöltése</h2>
+									<p>Szűrőfelület: kerület/lakótelep</p>
+									<ul class="actions">
+										<li><a href="lakotelep.html" class="button">Szűrés</a></li>
+									</ul>									
+								</div>
+							</div>
+						</section>						
+						
+						
+                    </section>
+
+
+
+
+
+
+                    
+				<!-- Five -->
+					<section id="five" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Kapcsolat</h2>
+							<p>Várjuk a jobbnál jobb ötleteket, észrevételeket az oldallal kapcsolatban</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="name">Név</label>
+												<input type="text" name="name" id="name" />
+											</div>
+											<div class="field half">
+												<label for="email">Email</label>
+												<input type="text" name="email" id="email" />
+											</div>
+											<div class="field">
+												<label for="message">Üzenet</label>
+												<textarea name="message" id="message" rows="5"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Üzenet küldése</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Cím</h3>
+											<span>12345 Somewhere Road #654<br />
+											Nashville, TN 00000-0000<br />
+											USA</span>
+										</li>
+										<li>
+											<h3>Email</h3>
+											<a href="#">user@untitled.tld</a>
+										</li>
+										<li>
+											<h3>Telefon</h3>
+											<span>(000) 000-0000</span>
+										</li>										
+									</ul>
+								</section>
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d86286.48371794992!2d18.995707677701716!3d47.48109537927611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c334d1d4cfc9%3A0x400c4290c1e1160!2sBudapest!5e0!3m2!1shu!2shu!4v1681409731062!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+        <footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; Panelprojekt. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
