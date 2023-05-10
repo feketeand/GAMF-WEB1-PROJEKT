1cf1efd0fd70d90d3c64649283ef0e51037348a7
feketeand <130245144+feketeand@users.noreply.github.com>
2023-05-10 23:42:20 +0200

    Update belepes.tpl.php

--- templates/pages/belepes.tpl.php
+++ templates/pages/belepes.tpl.php
@@ -1,23 +1,29 @@
-    <form action = "?oldal=belep2" method = "post">
+   
+<section class="wrapper style2 spotlights">  
+    <form class="urlap" action = "belep2.php" method = "post">
       <fieldset>
-        <legend>Bejelentkezés</legend>
-        <br>
-        <input type="text" name="felhasznaloNev" placeholder="felhasználó" required><br><br>
-        <input type="password" name="jelszo" placeholder="jelszó" required><br><br>
-        <input type="submit" name="belepes" value="Belépés">
+        <h3>Bejelentkezés<h3>
+        <br>                    
+          <input type="text" name="felhasznaloNev" placeholder="felhasználó" required><br>
+          <input type="password" name="jelszo" placeholder="jelszó" required><br><br>
+          <input type="submit" name="belepes" value="Belépés" href="index.php?belep">                               
         <br>&nbsp;
       </fieldset>
     </form>
-    <h3>Regisztrálja magát, ha még nem felhasználó!</h2>
-    <form action = "?oldal=regisztral2" method = "post">
-    <fieldset>
-        <legend>Regisztráció</legend>
+
+    
+    <form action = "regisztral2.php" method = "post">
+      <fieldset>
+        <legend>Regisztrálja magát, ha még nem felhasználó!</legend>
+        <h3>Regisztráció</h3>
         <br>
         <input type="text" name="vezeteknev" placeholder="vezetéknév" required><br>
         <input type="text" name="keresztnev" placeholder="keresztnév" required><br>
         <input type="text" name="felhasznaloNev" placeholder="felhasználó név" required><br>
         <input type="password" name="jelszo" placeholder="jelszó" required><br>
-        <input type="submit" name="regisztracio" value="Regisztráció">
+        <input type="submit" name="regisztracio" value="Regisztráció" href="index.php?regisztral">
         <br>&nbsp;
       </fieldset>
     </form>
+  
+</section>
