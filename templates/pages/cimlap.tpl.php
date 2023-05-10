680096626cf53c2a353fb01fd6534142d6cf8562
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-10 13:55:55 +0200

    Some changes

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -67,8 +67,6 @@
 	<section id="three" class="wrapper style2 spotlights">
 		<section>
 			<video width="400" height="450" controls>
-				<source src="movie.mp4" type="video/mp4">
-				<source src="movie.ogg" type="video/ogg">
 				Your browser does not support the video tag.
 			</video>
 			<div class="content">
