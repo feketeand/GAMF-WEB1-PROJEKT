11c6a584ae9c5c42ac1ca15170e5966836dda967
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-10 10:56:24 +0200

    Some updates in cimlap.tpl.php

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -49,8 +49,52 @@
 
 
 	</section>
-	<section class="wrapper style2 spotlights">
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d86286.48371794992!2d18.995707677701716!3d47.48109537927611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c334d1d4cfc9%3A0x400c4290c1e1160!2sBudapest!5e0!3m2!1shu!2shu!4v1681409731062!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	<section id="three" class="wrapper style2 spotlights">
+		<section>
+			<iframe width="400" height="450">
+			</iframe>
+			<div class="content">
+				<div class="inner">
+					<h2>Panelos videó</h2>
+					<p>Panellakásról videó</p>
+					<ul class="actions">
+						<li><a href="lakotelep.html" class="button">Szűrés</a></li>
+					</ul>
+				</div>
+			</div>
+		</section>
+	</section>
+	<section id="three" class="wrapper style2 spotlights">
+		<section>
+			<video width="400" height="450" controls>
+				<source src="movie.mp4" type="video/mp4">
+				<source src="movie.ogg" type="video/ogg">
+				Your browser does not support the video tag.
+			</video>
+			<div class="content">
+				<div class="inner">
+					<h2>Panelos videó</h2>
+					<p>Panellakásról videó</p>
+					<ul class="actions">
+						<li><a href="lakotelep.html" class="button">Szűrés</a></li>
+					</ul>
+				</div>
+			</div>
+		</section>
+	</section>
+	<section id="three" class="wrapper style2 spotlights">
+		<section>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d86286.48371794992!2d18.995707677701716!3d47.48109537927611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c334d1d4cfc9%3A0x400c4290c1e1160!2sBudapest!5e0!3m2!1shu!2shu!4v1681409731062!5m2!1shu!2shu" width="400" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" data-position="center center"></iframe>
+			<div class="content">
+				<div class="inner">
+					<h2>Térkép</h2>
+					<p>Panelprojekt címe</p>
+					<ul class="actions">
+						<li><a href="lakotelep.html" class="button">Szűrés</a></li>
+					</ul>
+				</div>
+			</div>
+		</section>
 	</section>
 
 </div>
@@ -61,4 +105,4 @@
 <script src="assets/js/browser.min.js"></script>
 <script src="assets/js/breakpoints.min.js"></script>
 <script src="assets/js/util.js"></script>
-<script src="assets/js/main.js"></script>  
+<script src="assets/js/main.js"></script>
