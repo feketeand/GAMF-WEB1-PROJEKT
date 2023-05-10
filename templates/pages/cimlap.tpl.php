67a7de3a6cc88a6f3ad957e9be04d81c52dcd199
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-10 18:05:01 +0200

    Update and add files

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -6,52 +6,18 @@
 	<section id="intro" class="wrapper style1 fullscreen fade-up">
 		<div class="inner">
 			<h1>Panelprojekt</h1>
-			<p>Üdvözöljük</p>
+			<p>Üdvözöljük a Panelprojekt weboldalán!</p>
+			<p>Panellakásokról és lakótelepekről lehet információkat gyűjteni.</p>
 		</div>
 	</section>
 
 
-	<!-- Lakótelep kereső -->
-
-	<section id="two" class="wrapper style2 spotlights">
-		<section>
-			<a href="#" class="image"><img src="images/kep3.jpg" style="max-width:500px;" alt="" data-position="center center" /></a>
-			<div class="content">
-				<div class="inner">
-					<h2>Lakótelep kereső</h2>
-					<p>Szűrőfelület: kerület/lakótelep</p>
-					<ul class="actions">
-						<li><a href="lakotelep.html" class="button">Szűrés</a></li>
-					</ul>
-				</div>
-			</div>
-		</section>
-
-
-	</section>
-
-
-	<!-- Lakáskereső -->
-
-	<section id="three" class="wrapper style2 spotlights">
-		<section>
-			<a href="#" class="image"><img src="images/kep3.jpg" style="max-width:500px;" alt="" data-position="center center" /></a>
-			<div class="content">
-				<div class="inner">
-					<h2>Lakástípus kereső</h2>
-					<p>Szűrőfelület: kerület/lakótelep/méret/szobaszám/külön wc/ablakos konyha</p>
-					<ul class="actions">
-						<li><a href="lakotelep.html" class="button">Szűrés</a></li>
-					</ul>
-				</div>
-			</div>
-		</section>
 
 
 	</section>
 	<section id="three" class="wrapper style2 spotlights">
 		<section>
-			<iframe width="400" height="450">
+			<iframe max-width="500" height="300" src="https://assets.mixkit.co/videos/preview/mixkit-european-style-buildings-4352-large.mp4">
 			</iframe>
 			<div class="content">
 				<div class="inner">
@@ -66,13 +32,13 @@
 	</section>
 	<section id="three" class="wrapper style2 spotlights">
 		<section>
-			<video width="400" height="450" controls>
-				Your browser does not support the video tag.
+			<video width="300" height="300" controls>
+				<source src="video.mp4" type="video/mp4">
 			</video>
 			<div class="content">
 				<div class="inner">
-					<h2>Panelos videó</h2>
-					<p>Panellakásról videó</p>
+					<h2>Lakótelepes videó</h2>
+					<p>Lakótelepekről videó</p>
 					<ul class="actions">
 						<li><a href="lakotelep.html" class="button">Szűrés</a></li>
 					</ul>
@@ -82,7 +48,7 @@
 	</section>
 	<section id="three" class="wrapper style2 spotlights">
 		<section>
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d86286.48371794992!2d18.995707677701716!3d47.48109537927611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c334d1d4cfc9%3A0x400c4290c1e1160!2sBudapest!5e0!3m2!1shu!2shu!4v1681409731062!5m2!1shu!2shu" width="400" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" data-position="center center"></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d86286.48371794992!2d18.995707677701716!3d47.48109537927611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c334d1d4cfc9%3A0x400c4290c1e1160!2sBudapest!5e0!3m2!1shu!2shu!4v1681409731062!5m2!1shu!2shu" max-width="400" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" data-position="center center"></iframe>
 			<div class="content">
 				<div class="inner">
 					<h2>Térkép</h2>
