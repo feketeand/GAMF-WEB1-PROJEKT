044c6e0a86150615fbca3fe231ae81a2f2a07ea5
feketeand <130245144+feketeand@users.noreply.github.com>
2023-05-10 23:40:26 +0200

    Update config.inc.php

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -16,15 +16,21 @@ $lablec = array(
 );
 
 $oldalak = array(
-	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Kezdőlap'),
-	'panellakasokrol' => array('fajl' => 'panellakasokrol', 'szoveg' => 'Panellakásokról általában'),
-	'lakotelepek' => array('fajl' => 'lakotelepek', 'szoveg' => 'Lakótelepek Budapesten'),
-	'lakaskereso' => array('fajl' => 'lakaskereso', 'szoveg' => 'Lakáskereső'),
-	'otletfeltolto' => array('fajl' => 'otletfelto', 'szoveg' => 'Ötlet feltöltés'),
-	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat'),
-	'belepes' => array('fajl' => 'belep2', 'szoveg' => 'Belépés/Regisztráció'),
-	'kilepes' => array('fajl' => 'kilep2', 'szoveg' => 'Kilépés')
+	'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés/Regisztráció', 'menun' => array(1,0)),
+	'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Kezdőlap', 'menun' => array(1,1)),
+	'panellakasokrol' => array('fajl' => 'panellakasokrol', 'szoveg' => 'Panellakásokról általában', 'menun' => array(1,1)),
+	'lakotelepek' => array('fajl' => 'lakotelepek', 'szoveg' => 'Lakótelepek Budapesten', 'menun' => array(1,1)),
+	'lakaskereso' => array('fajl' => 'lakaskereso', 'szoveg' => 'Lakáskereső', 'menun' => array(1,1)),
+	'otletfeltolto' => array('fajl' => 'otletfelto', 'szoveg' => 'Ötlet feltöltés', 'menun' => array(1,1)),
+	'otletfeltolto2' => array('fajl' => 'otletfelto', 'szoveg' => 'Ötlet feltöltés2', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+	'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))			
 );
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
 
 $hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
 ?>
