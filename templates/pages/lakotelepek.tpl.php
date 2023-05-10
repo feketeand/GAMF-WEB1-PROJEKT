7d572ec071f7770cbfb5d4890c6cd3c4c42a5d26
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-10 18:03:59 +0200

    Update lakotelepek.tpl.php

---
+++ templates/pages/lakotelepek.tpl.php
@@ -0,0 +1,34 @@
+<!-- Lakótelep kereső -->
+
+<section id="two" class="wrapper style2 spotlights">
+    <section>
+        <a href="#" class="image"><img src="images/kep3.jpg" style="max-width:500px;" alt="" data-position="center center" /></a>
+        <div class="content">
+            <div class="inner">
+                <h2>Lakótelep kereső</h2>
+                <p>Szűrőfelület: kerület/lakótelep</p>
+                <ul class="actions">
+                    <li><a href="lakotelep.html" class="button">Szűrés</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+
+</section>
+
+<!-- Lakáskereső -->
+
+<section id="three" class="wrapper style2 spotlights">
+    <section>
+        <a href="#" class="image"><img src="images/kep3.jpg" style="max-width:500px;" alt="" data-position="center center" /></a>
+        <div class="content">
+            <div class="inner">
+                <h2>Lakástípus kereső</h2>
+                <p>Szűrőfelület: kerület/lakótelep/méret/szobaszám/külön wc/ablakos konyha</p>
+                <ul class="actions">
+                    <li><a href="lakotelep.html" class="button">Szűrés</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
