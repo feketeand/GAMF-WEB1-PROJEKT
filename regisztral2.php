6fb9b4103c6b9234719b6495ac68f9b1f1fbf0ca
feketeand <130245144+feketeand@users.noreply.github.com>
2023-05-11 07:14:19 +0200

    Add files via upload

--- regisztral2.php
+++ regisztral2.php
@@ -1,5 +1,7 @@
 <?php
+
 if(isset($_POST['felhasznaloNev']) && isset($_POST['jelszo']) && isset($_POST['vezeteknev']) && isset($_POST['keresztnev'])) {
+    
     try {
         // Kapcsolódás
         $dbh = new PDO('mysql:host=localhost;dbname=paneldb', 'root', '',
@@ -7,24 +9,31 @@ if(isset($_POST['felhasznaloNev']) && isset($_POST['jelszo']) && isset($_POST['v
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Létezik már a felhasználói név?
-        $sqlSelect = "select id from felhasznalok where felhasznaloNev = :felhasznaloNev";
+        $sqlSelect = "select felhasznaloID from felhasznalo where felhasznaloNev = :felhasznaloNev";
         $sth = $dbh->prepare($sqlSelect);
-        $sth->execute(array(':felhasznaloNev' => $_POST['felhasznalo']));
+        $sth->execute(array(':felhasznaloNev' => $_POST['felhasznaloNev']));
+        
+
         if($row = $sth->fetch(PDO::FETCH_ASSOC)) {
             $uzenet = "A felhasználói név már foglalt!";
             $ujra = "true";
+            
         }
+        
         else {
+            
             // Ha nem létezik, akkor regisztráljuk
-            $sqlInsert = "insert into felhasznalok(id, vezeteknev, keresztnev, felhasznaloNev, jelszo)
-                          values(0, :vezeteknev, :vezeteknev, :felhasznaloNev, :jelszo)";
+            $sqlInsert = "insert into felhasznalo(felhasznaloID, vezeteknev, keresztnev, felhasznaloNev, jelszo)
+                          values(0, :vezeteknev, :keresztnev, :felhasznaloNev, :jelszo)";
             $stmt = $dbh->prepare($sqlInsert); 
             $stmt->execute(array(':vezeteknev' => $_POST['vezeteknev'], ':keresztnev' => $_POST['keresztnev'],
                                  ':felhasznaloNev' => $_POST['felhasznaloNev'], ':jelszo' => sha1($_POST['jelszo']))); 
+            
             if($count = $stmt->rowCount()) {
                 $newid = $dbh->lastInsertId();
                 $uzenet = "A regisztrációja sikeres.<br>Azonosítója: {$newid}";                     
                 $ujra = false;
+                
             }
             else {
                 $uzenet = "A regisztráció nem sikerült.";
@@ -41,3 +50,20 @@ else {
     header("Location: .");
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Regisztráció</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php if(isset($uzenet)) { ?>
+            <h1><?= $uzenet ?></h1>
+            <?php if($ujra) { ?>
+                <a href="index.php?oldal=belepes">Próbálja újra!</a>
+            <?php } ?>
+        <?php } ?>
+        <a href="index.php?/"> Tovább a kezdőlapra</a>
+    </body>  
+</html>
