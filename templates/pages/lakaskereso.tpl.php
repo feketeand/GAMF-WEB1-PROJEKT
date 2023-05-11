d622db3354b9667601df531bf1d7d3935c38e09c
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-11 14:25:28 +0200

    Add files via upload

--- templates/pages/lakaskereso.tpl.php
+++ templates/pages/lakaskereso.tpl.php
@@ -1,19 +1,15 @@
-<!-- Lakótelep kereső -->
+<!-- Lakáskereső -->
 
-<section id="two" class="wrapper style2 spotlights">
+<section id="three" class="wrapper style2 spotlights">
     <section>
         <a href="#" class="image"><img src="images/kep3.jpg" style="max-width:500px;" alt="" data-position="center center" /></a>
         <div class="content">
             <div class="inner">
-                <h2>Lakótelep kereső</h2>
-                <p>Szűrőfelület: kerület/lakótelep</p>
+                <h2>Lakástípus kereső</h2>
+                <p>Szűrőfelület: kerület/lakótelep/méret/szobaszám/külön wc/ablakos konyha</p>
                 <ul class="actions">
                     <li><a href="lakotelep.html" class="button">Szűrés</a></li>
                 </ul>
             </div>
         </div>
     </section>
-
-
-</section>
-
