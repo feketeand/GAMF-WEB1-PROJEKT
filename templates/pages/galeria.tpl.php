af6f6138cd5d6aa120c4014323f5e0d0448b7727
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-12 19:09:35 +0200

    Update galeria.tpl.php

--- templates/pages/galeria.tpl.php
+++ templates/pages/galeria.tpl.php
@@ -12,6 +12,29 @@ while (($fajl = readdir($olvaso)) !== false) {
 }
 closedir($olvaso);
 
+
+if (isset($_POST['kuldes'])) {
+    foreach ($_FILES as $fajl) {
+        if ($fajl['error'] == 4);
+        elseif (!in_array($fajl['type'], $MEDIATIPUSOK))
+            $uzenet[] = " Nem megfelelő típus: " . $fajl['name'];
+        elseif (
+            $fajl['error'] == 1
+            or $fajl['error'] == 2
+        )
+            $uzenet[] = " Túl nagy állomány: " . $fajl['name'];
+        else {
+            $vegsohely = $MAPPA . strtolower($fajl['name']);
+            if (file_exists($vegsohely))
+                $uzenet[] = " Már létezik: " . $fajl['name'];
+            else {
+                move_uploaded_file($fajl['tmp_name'], $vegsohely);
+                $uzenet[] = ' Ok: ' . $fajl['name'];
+            }
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,13 +46,25 @@ closedir($olvaso);
 
 <body>
     <section id="intro" class="wrapper style1 fullscreen fade-up">
-        <div id="galeria"> 
+        <div id="galeria">
             <h1 class="inner">Galéria</h1>
+            <h2>Feltöltés a galériába:</h2>
+            <?php
+            if (!empty($uzenet)) {
+                echo '<ul>';
+                foreach ($uzenet as $uz)
+                    echo "<li>$uz</li>";
+                echo '</ul>';
+            }
+            ?>
+            <form method="post" enctype="multipart/form-data">
+                <input type="file" name="elso" required>
+                <input type="submit" name="kuldes">
+            </form>
             <?php
             foreach ($kepek as $fajl => $datum) {
             ?>
-                <!--<a href="<?php echo $MAPPA . $fajl ?>">-->
-                    <img src="<?php echo $MAPPA . $fajl ?>" alt="" data-position="center center" width="490" height="500">
+                <img src="<?php echo $MAPPA . $fajl ?>" alt="" data-position="center center" width="490" height="500">
             <?php
             }
             ?>
