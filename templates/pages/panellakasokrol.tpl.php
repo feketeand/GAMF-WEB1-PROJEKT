e1c17eadcb95ade31540f87f128e167dde4632fc
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-12 06:55:01 +0200

    Updates

--- templates/pages/panellakasokrol.tpl.php
+++ templates/pages/panellakasokrol.tpl.php
@@ -1,21 +1,28 @@
 <!-- Panellakásokról -->
 <section id="one" class="wrapper style2 spotlights">
-        <section>
-            <a href="#" class="image"><img src="images/kep1.jpg" alt="" data-position="center center" /></a>
-            <div class="content">
-                <div class="inner">
-                    <h2> leírás</h2>
-                    <p>Szöveg</p>									
-                </div>
+    <section>
+        <a href="#" class="image"><img src="images/kep1.jpg" alt="" data-position="center center" /></a>
+        <div class="content">
+            <div class="inner">
+                <h2>Panellakások</h2>
+                <p>A panelház vagy panelépület előre gyártott vasbeton elemekből készült épülettömb.
+                    Magassága Magyarországon átlagosan 20-30 méter.
+                    Jellemzően a lakótelepek többemeletes lakótömbjeit építették ezzel a technológiával, de épültek ilyen módon
+                    szállodák és irodaházak is.
+                    Viszonylag gyors és olcsó építési technológia.
+                    Magyarországon közel 788 ezer lakás található panelépületben, ezekben él a lakosság ötöde. </p>
             </div>
-        </section>
-        <section>
-            <a href="#" class="image"><img src="images/kép2.jpg" alt="" data-position="top center" /></a>
-            <div class="content">
-                <div class="inner">
-                    <h2>További cikkek a témában</h2>
-                    <p>Link lista</p>									
-                </div>
-            </div>
-        </section>						
+        </div>
     </section>
+    <section>
+        <a href="#" class="image"><img src="images/kép2.jpg" alt="" data-position="top center" /></a>
+        <div class="content">
+            <div class="inner">
+                <h2>További cikkek a témában</h2>
+                <ul>
+                <a style="color: black;" href="https://www.origo.hu/gazdasag/20200130-60-eve-adtak-at-az-elso-panelhazat.html"><p>Paneltörténelem Magyarországon</p></a>
+                </ul>
+            </div>
+        </div>
+    </section>
+</section>
