82fa8323fe9f2322f51ecdeaadc1d11bdfd1c915
feketeand <130245144+feketeand@users.noreply.github.com>
2023-05-14 17:46:11 +0200

    Add files via upload

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -14,9 +14,7 @@ $lablec = array(
 	'oldal' => 'Panelprojekt'
 );
 
-$oldalak = array(
-	'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés/Regisztráció', 'menun' => array(1, 0)),
-	'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0, 1)),
+$oldalak = array(	
 	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Kezdőlap', 'menun' => array(1, 1)),
 	'panellakasokrol' => array('fajl' => 'panellakasokrol', 'szoveg' => 'Panellakásokról általában', 'menun' => array(1, 1)),
 	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria', 'menun' => array(1, 1)),
@@ -24,13 +22,20 @@ $oldalak = array(
 	'lakaskereso' => array('fajl' => 'lakaskereso', 'szoveg' => 'Lakáskereső', 'menun' => array(1, 1)),
 	'otletfeltolto' => array('fajl' => 'otletfelto', 'szoveg' => 'Ötlet feltöltés', 'menun' => array(1, 1)),
 	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1, 1)),
+	'karbantarto'=> array('fajl' => 'karbantarto', 'szoveg' => 'Karbantartó', 'menun' => array(1, 1)),
+	'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés/Regisztráció', 'menun' => array(1, 0)),
+	'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0, 1)),
 	'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0, 0)),
-	'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0, 0))
+	'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0, 0)),
+	'ltpmentes' => array('fajl' => 'ltpmentes', 'szoveg' => '', 'menun' => array(0, 0)),
+	'tipmentes' => array('fajl' => 'tipmentes', 'szoveg' => '', 'menun' => array(0, 0)),
+	'alaprajzok' => array('fajl' => 'tipmentes', 'szoveg' => '', 'menun' => array(0, 0))
 );
 
 $hiba_oldal = array('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
 
 $MAPPA = 'images/';
+$MAPPA2 = 'images/alaprajzok/';
 $TIPUSOK = array('.jpg', '.png');
 $MEDIATIPUSOK = array('image/jpeg', 'image/png');
 ?>
