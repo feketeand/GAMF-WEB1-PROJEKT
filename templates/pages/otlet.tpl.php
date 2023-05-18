7c4bfbc40571acbf91d22218c15bb57e5d60f4c1
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-18 22:30:19 +0200

    Add files via upload

---
+++ templates/pages/otlet.tpl.php
@@ -0,0 +1,44 @@
+<?php include ('lakasok.tpl.php') ?>
+<?php for($i=0;$i<count($result);$i++) {
+$lakasID = $result[$i][0]; }	?>
+<h1> Új ötlet feltöltése a <?php echo $lakasID?> lakástípushoz: </h1>
+	<form method = "post">
+      <fieldset>        
+        <br>
+        <label> Megnevezés </label> <input type="text" name="megnev" placeholder="Megnevezés" required><br><br>
+		<label> Leírás </label> <input type="textarea" rows="5" name="leiras" placeholder="Leírás" required><br><br>
+		<label> Új alaprajz ID <input type="text" name="ujarID" value="0" required><br><br>
+		<input type="submit" name="mentes" value="Mentés">        	
+        <br>&nbsp;
+      </fieldset>
+    </form>
+	
+<?php
+if(isset($_POST['megnev']) && isset($_POST['leiras'])) {
+    try {
+        // Kapcsolódás
+        $dbh = new PDO('mysql:host=localhost;dbname=paneldb', 'root', '',
+                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');                     
+                        
+        $sqlInsert = "insert into otlet(otletID, felhasznaloID, lakastipusID, megnevezes, leiras, alaprajzID)
+                          values(0, :felhID, :tipusID, :megnev, :leir, :ujarID)";
+            $stmt = $dbh->prepare($sqlInsert); 
+            $stmt->execute(array(':felhID' => $_SESSION['login'],':tipusID' => $lakasID, ':megnev' => $_POST['megnev'],
+                                 ':leir' => $_POST['leiras'],':ujarID' => $_POST['ujarID'])); 
+            if($count = $stmt->rowCount()) {
+                $newid = $dbh->lastInsertId();
+                $uzenet = "A mentés sikeres.<br>Azonosító: {$newid}";                     
+                $ujra = false;
+            }
+            else {
+                $uzenet = "A mentés nem sikerült.";
+                $ujra = true;
+            }
+        }    
+    catch (PDOException $e) {
+        $uzenet = "Hiba: ".$e->getMessage();
+        $ujra = true;
+    }	
+}
+?>
--- templates/pages/otletek.tpl.php
+++ templates/pages/otletek.tpl.php
@@ -1,10 +1,17 @@
 <body>
-
 <?php include ('lakasok.tpl.php') ?>
-<?php echo $result[$i][0];	?>
+<table>                			
+                <?php for($i=0;$i<count($result);$i++){?>
+				<tr>
+                    <td><?php echo($result[$i][0])?></td>				
+                </tr><br>                
+                <?php $lakasID = $result[$i][0]; } ?> 
+            </table>
 <?php
 	try {
+        for($i=0;$i<count($result);$i++) {
 		echo $result[$i][0];
+        }
         // Kapcsolódás
         $dbh = new PDO('mysql:host=localhost;dbname=paneldb', 'root', '',
                         array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
@@ -42,51 +49,8 @@
                     <td><?php echo($res[$i][1])?></td>
                     <td><?php echo($res[$i][3])?></td>
                     <td><?php echo($res[$i][5])?></td>									
-					<td><a href="alaprajzok.php"><?php echo($res[$i][4])?></a></td>												
+					<td><a href="alaprajzok.php"  target="_blank"><?php echo($res[$i][4])?></a></td>												
                 </tr><br>                
                 <?php } ?>
 				
             </table>
-
-			
-<h1> Új ötlet feltöltése a <?php echo $lakasID?> lakástípushoz: </h1>
-	<form method = "post">
-      <fieldset>        
-        <br>
-        <label> Megnevezés </label> <input type="text" name="megnev" placeholder="Megnevezés" required><br><br>
-		<label> Leírás </label> <input type="textarea" rows="5" name="leiras" placeholder="Leírás" required><br><br>
-		<label> Új alaprajz ID <input type="text" name="ujarID" value="0" required><br><br>
-		<input type="submit" name="mentes" value="Mentés">        	
-        <br>&nbsp;
-      </fieldset>
-    </form>
-	
-<?php
-if(isset($_POST['megnev']) && isset($_POST['leiras'])) {
-    try {
-        // Kapcsolódás
-        $dbh = new PDO('mysql:host=localhost;dbname=paneldb', 'root', '',
-                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-        $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');                     
-                        
-        $sqlInsert = "insert into otlet(otletID, felhasznaloID, lakastipusID, megnevezes, leiras, alaprajzID)
-                          values(0, :felhID, :tipusID, :megnev, :leir, :ujarID)";
-            $stmt = $dbh->prepare($sqlInsert); 
-            $stmt->execute(array(':felhID' => $_SESSION['login'],':tipusID' => $lakasID, ':megnev' => $_POST['megnev'],
-                                 ':leir' => $_POST['leiras'],':ujarID' => $_POST['ujarID'])); 
-            if($count = $stmt->rowCount()) {
-                $newid = $dbh->lastInsertId();
-                $uzenet = "A mentés sikeres.<br>Azonosító: {$newid}";                     
-                $ujra = false;
-            }
-            else {
-                $uzenet = "A mentés nem sikerült.";
-                $ujra = true;
-            }
-        }    
-    catch (PDOException $e) {
-        $uzenet = "Hiba: ".$e->getMessage();
-        $ujra = true;
-    }	
-}
-?>
