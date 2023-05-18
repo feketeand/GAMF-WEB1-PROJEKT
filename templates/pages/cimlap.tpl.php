07d8eac96c33645cd45150d2aabb8b8cb7e4ada3
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-05-18 20:14:21 +0200

    Update cimlap.tpl.php

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -1,10 +1,9 @@
-<!-- Intro -->
-					<section id="intro" class="wrapper style1 fullscreen fade-up">
-						<div class="inner">
-							<h1>Panelprojekt</h1>
-							<p>Üdvözöljük a weboldalon</p>					
-						</div>
-					</section>
-					<video controls>
-					<source src="video.mp4" type="video/mp4">
-					</video>
+<section>
+	<div class="inner">
+		<h1>Panelprojekt</h1>
+		<p>Üdvözöljük a weboldalon</p>
+	</div>
+</section>
+<video controls>
+	<source src="video.mp4" type="video/mp4">
+</video>
